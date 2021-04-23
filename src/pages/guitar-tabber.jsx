30a202103c778b23e8dab95f219bbb4be7490a33
Devin <devin.quirk@velir.com>
2021-04-22 22:15:43 -0400

    moving guitar tabber to sub directory

--- src/pages/guitar-tabber.jsx
+++ src/pages/guitar-tabber.jsx
@@ -2,12 +2,14 @@ import * as React from "react";
 
 import MainLayout from "../components/MainLayout/MainLayout.jsx";
 import Seo from "../components/Seo/Seo.jsx";
+import Tabber from "../components/Tabber/Tabber.jsx";
 
-const IndexPage = () => (
+const GuitarTabberPage = () => (
   <MainLayout>
     <Seo title="Guitar Tabber" />
-    <p>Welcome to my website. More to come soon.</p>
+    <p>Welcome to guitar tabber. I'm testing if this is useful.</p>
+    <Tabber />
   </MainLayout>
 );
 
-export default IndexPage;
+export default GuitarTabberPage;
