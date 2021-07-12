d8522679e81a3c6b357cce70e4ccd3b247d0ac0c
Devin <devin.quirk@velir.com>
2021-07-11 23:26:16 -0400

    config cleanup

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,22 +4,14 @@ require("dotenv").config({
 
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Default Starter",
-    description:
-      "Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.",
-    author: "@gatsbyjs",
+    title: "Guitar Tabber",
+    description: "Quickly make and share a guitar tab.",
+    author: "Devin Quirk",
   },
   plugins: [
     "gatsby-plugin-sass",
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-image",
-    // {
-    //   resolve: "gatsby-source-contentful",
-    //   options: {
-    //     spaceId: process.env.SPACE_ID,
-    //     accessToken: process.env.CONTENT_DELIVERY_API,
-    //   },
-    // },
     {
       resolve: "gatsby-source-filesystem",
       options: {
@@ -33,8 +25,8 @@ module.exports = {
     {
       resolve: "gatsby-plugin-manifest",
       options: {
-        name: "gatsby-starter-default",
-        short_name: "starter",
+        name: "guitar-tabber",
+        short_name: "tabber",
         start_url: "/",
         background_color: "#663399",
         theme_color: "#663399",
@@ -42,9 +34,5 @@ module.exports = {
         icon: "src/images/gatsby-icon.png", // This path is relative to the root of the site.
       },
     },
-    // "gatsby-plugin-gatsby-cloud",
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
 };
