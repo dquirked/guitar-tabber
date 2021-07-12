c5e54ceb520953ce2c4ea13976941a1104a5e2d0
Devin <devin.quirk@velir.com>
2021-07-11 23:22:57 -0400

    fresh start

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,14 +1,5 @@
 import * as React from "react";
 
-import MainLayout from "../components/MainLayout/MainLayout.jsx";
-import Seo from "../components/Seo/Seo.jsx";
-import { Link } from "gatsby";
-
-const IndexPage = () => (
-  <MainLayout>
-    <Seo title="Devin's Website" />
-    <Link to="/guitar-tabber">Check out what I'm working on.</Link>
-  </MainLayout>
-);
+const IndexPage = () => <div>index page</div>;
 
 export default IndexPage;
