989017309df5e4a84f682ca2800eaf87b493ac12
Devin <devin.quirk@velir.com>
2021-07-16 13:01:00 -0400

    addings strings and beats

--- src/components/Bar/Bar.jsx
+++ src/components/Bar/Bar.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useState, useMemo } from "react";
+import * as R from "ramda";
+import "./bar.scss";
 
 const Bar = () => {
-  return <div className="bar"></div>;
+  const [strings, setStrings] = useState(["e", "B", "G", "D", "A", "E"]);
+  const [beatsPerMeasure, setBeatsPerMeasure] = useState(4);
+  const [noteType, setNoteType] = useState("eigth");
+
+  const beats = useMemo(() => {
+    return R.range(0, beatsPerMeasure);
+  }, [beatsPerMeasure]);
+
+  return (
+    <div className="bar">
+      {strings.map((string, i) => (
+        <div key={string} className="bar__string">
+          {beats.map((beat, i) => (
+            <div key={beat}>
+              <input type="text" />
+            </div>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Bar;
